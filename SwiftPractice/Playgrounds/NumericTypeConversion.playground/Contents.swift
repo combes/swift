07c11365f6f9225920ba8829a0332a20b8c25b4e
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2016-10-29 18:34:52 -0500

    Create FoodTracker project based on Swift lesson.

---
+++ SwiftPractice/Playgrounds/NumericTypeConversion.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+// Overflow
+// let cannotBeNegative: UInt8 = -1 // Cannot store negative value in a type of UInt
+// let tooBig: Int8 = Int8.max + 1 // Overflow
+// let tooSmall: Int8 = Int8.min - 1 // Overflow
+
+// SomeType(ofInitialValue) is the default way to call the initializer of a Swift type and pass in an initial value.
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
+// Integer and floating-point conversion
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+let integerPi = Int(pi)
+let value = 3 + 0.14159 // Type is inferred during the compilation phase
+
+// Type aliases
+typealias AudioSample = UInt16
+var maxAmplitude = AudioSample.min
+
