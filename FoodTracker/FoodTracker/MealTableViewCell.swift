41d65d01b282ed842acc3fd50fd2403117efeccb
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2016-11-09 19:40:34 -0600

    Added table view and meal prototype cell.

---
+++ FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Christopher Combes on 11/9/16.
+//  Copyright © 2016 Christopher Combes. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
