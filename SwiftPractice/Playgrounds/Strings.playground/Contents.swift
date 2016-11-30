904eaf872f0b0cfc514a6a29a58a7864fdc9226c
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2016-11-30 12:14:46 -0600

    Learning about Character and String types.

--- SwiftPractice/Playgrounds/Strings.playground/Contents.swift
+++ SwiftPractice/Playgrounds/Strings.playground/Contents.swift
@@ -17,3 +17,62 @@ let constantString = "Highlander"
 
 // Strings are Value Types
 // Passing a string makes a copy and therefore modifications are local and do not mutate the original object.
+
+// Working with Characters
+for character in "Doggie! 🐶".characters {
+    print(character)
+}
+
+// Stand-alone Character constant
+let exclamationMark: Character = "!"
+
+// Build String from Character array
+let catCharacters: [Character] = ["C", "a", "t", "!", "🐱"]
+let catString = String(catCharacters)
+print(catString)
+
+// String concatenation
+let string1 = "Hello"
+let string2 = " there"
+var welcome = string1 + string2
+
+var instruction = "look over"
+instruction += string2
+
+// Appending Character to a String
+welcome.append(exclamationMark)
+
+// String Interpolation
+let multiplier = 3
+let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
