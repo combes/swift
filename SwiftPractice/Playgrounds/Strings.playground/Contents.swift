2ca8bced0c1562d7c362d7b85afcd05d2077fe82
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2016-12-01 12:39:53 -0600

    Studying Unicode characters and string indexing.

--- SwiftPractice/Playgrounds/Strings.playground/Contents.swift
+++ SwiftPractice/Playgrounds/Strings.playground/Contents.swift
@@ -46,19 +46,64 @@ welcome.append(exclamationMark)
 let multiplier = 3
 let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
 
+// Unicode
+// String type is built from Unicode scalar values.
+// A Unicode scalar is a unique 21-bit number for a character.
 
+let wiseWords = "\"Wherever you go, there you are\""
+let dollarSign = "\u{24}"
+let blackHeart = "\u{2665}"
+let sparklingHeart = "\u{1F496}"
 
+// An extended grapheme cluster is a sequence of one or more Unicode scalars that
+// (when combined)  produce a single human-readable character.
+let eAcute: Character = "\u{E9}"
+let combinedEAcute: Character = "\u{65}\u{301}"
+let precomposed: Character = "\u{D55C}"
+let decomposed: Character = "\u{1112}\u{1161}\u{11AB}"
+let enclosedEAcute: Character = "\u{E9}\u{20DD}"
 
+// Unicode scalars for regional indicator symbols can be combined in pairs
+// to make a single Character value.
+let regionalIndicatorForUS: Character = "\u{1F1FA}\u{1F1F8}"
 
+// Counting characters
+let unusualMenagerie = "Koala 🐨, Snail 🐌, Penguin 🐧, Dromedary 🐪"
+print("unusualMenagerie has \(unusualMenagerie.characters.count) characters")
+var symbolMenagerie = "🐨🐌🐧🐪"
+symbolMenagerie.characters.count
 
+// Appending a diacritic will not change the word count.
+var word = "cafe"
+word.characters.count
+word += "\u{301}"
+word.characters.count
 
+// Performance note: Counting # of characters requires iteration in order to determine the grapheme clusters.
 
+// String indices
+let greeting = "Guten Tag!"
+greeting[greeting.startIndex]
+greeting[greeting.index(before: greeting.endIndex)]
+greeting[greeting.index(after: greeting.startIndex)]
+let index = greeting.index(greeting.startIndex, offsetBy: 7)
+greeting[index]
+// greeting[greeting.endIndex] // error
+// greeting.index(after: greeting.endIndex) // error
 
+// Use the indices property to access individual characters.
+for index in greeting.characters.indices {
+    print("\(greeting[index]) ", terminator: "")
+}
+// startIndex, endIndex, and other properties are available on Array, Dictionary, and Set.
 
-
-
-
-
+// Inserting and Removing Characters
+var hello = "hello"
+hello.insert("!", at: hello.endIndex)
+hello.insert(contentsOf: " there".characters, at: hello.index(before: hello.endIndex))
+hello.remove(at: hello.index(before: hello.endIndex))
+let range = hello.index(hello.endIndex, offsetBy: -6)..<hello.endIndex
+hello.removeSubrange(range)
 
 
 
