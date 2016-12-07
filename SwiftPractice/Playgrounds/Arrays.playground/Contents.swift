d4e1318a4dd909075089621c3ed5d75b282dbae4
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2016-12-07 14:37:46 -0600

    Learning about Array type.

---
+++ SwiftPractice/Playgrounds/Arrays.playground/Contents.swift
@@ -0,0 +1,54 @@
+//: Playground - noun: a place where people can play
+
+// Array Type Shorthand Syntax
+var someArray = Array<String>()
+var anotherArray = [String]()
+
+// Creating an Empty Array
+var someInts = [Int]()
+print("There are \(someInts.count) items.")
+someInts.append(3)
+someInts = [] // Empty but still of type [Int]
+
+// Creating an Array with a Default Value
+var threeDoubles = Array(repeating: 0.0, count: 3)
+
+// Creating an Array by Adding Two Arrays Together
+var anotherThreeDoubles = Array(repeating: 2.5, count: 3)
+var sixDoubles = threeDoubles + anotherThreeDoubles
+
+// Creating an Array with an Array Literal
+var shoppingList: [String] = ["Eggs", "Milk"]
+var shoppingListWithImplicitTyping = ["Eggs", "Milk"]
+
+// Accessing and Modifying an Array
+shoppingList.count
+shoppingList.isEmpty
+shoppingList.append("Flour")
+shoppingList += ["Baking Power", "Butter"]
+var firstItem = shoppingList[0]
+shoppingList[0] = "Six eggs"
+shoppingList[2...4] = ["Bananas", "Apples"]
+shoppingList
+shoppingList.insert("Maple Syrup", at: 0)
+let mapleSyrup = shoppingList.remove(at: 0)
+firstItem = shoppingList[0]
+let apples = shoppingList.removeLast()
+
+// Iterating Over an Array
+for item in shoppingList {
+    print(item)
+}
+
+for (index, value) in shoppingList.enumerated() {
+    print("Item \(index + 1): \(value)")
+}
+
+
+
+
+
+
+
+
+
