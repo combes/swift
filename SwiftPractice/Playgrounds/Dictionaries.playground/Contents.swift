a4013d7cd1a4fb9e35cd1ed947cea561ca03c732
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2016-12-09 16:23:11 -0600

    Learning about Dictionary type.

---
+++ SwiftPractice/Playgrounds/Dictionaries.playground/Contents.swift
@@ -0,0 +1,55 @@
+//: Playground - noun: a place where people can play
+
+var nameOfIntegers = [Int: String]()
+nameOfIntegers[16] = "sixteen"
+nameOfIntegers = [:] // Empty dictionary
+
+var airports: [String: String] = ["ATX" : "Austin-Bergstrom Airport",
+                                  "YYZ" : "Toronto Pearson",
+                                  "DUB" : "Dublin"]
+var airportsLiteral = ["ATX" : "Austin-Bergstrom Airport",
+                       "YYZ" : "Toronto Pearson",
+                       "DUB" : "Dublin"]
+airports.isEmpty
+airports["LHR"] = "London"
+airports["LHR"] = "London Heathrow" // Change value
+
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
+    print("The old value for DUB was \(oldValue).")
+}
+
+if let airportName = airports["DUB"] {
+    print("The name of the airport is \(airportName)")
+} else {
+    print("That airport is not in the airports dictionary")
+}
+
+airports["APL"] = "Apple International"
+airports["APL"] = nil
+
+if let removedValue = airports.removeValue(forKey: "DUB") {
+    print("The removed airport's name is \(removedValue).")
+} else {
+    print("The airports dictionary does not contain this value")
+}
+
+// Iterating Over a Dictionary
+for (airportCode, airportName) in airports {
+    print("\(airportCode): \(airportName)")
+}
+
+for airportCode in airports.keys {
+    print("Airport code: \(airportCode)")
+}
+
+for airportName in airports.values {
+    print("Airport name: \(airportName)")
+}
+
+let airportCodes = [String](airports.keys) // Initialize an array with Keys
+let airportNames = [String](airports.values) // Initialize an array with Values
+
+// Sorting
+for airportCode in airports.keys.sorted() {
+    print("Airport code: \(airportCode)")
+}
