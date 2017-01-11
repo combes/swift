2182116cbb530d80f25e6faf48e16c46c19f741c
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2017-01-11 12:03:12 -0600

    Learning about Swift Nested Types

---
+++ practice/Playgrounds/NestedTypes.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+// Nested Types in Action
+struct BlackjackCard {
+    // nested Suit enumeration
+    enum Suit: Character {
+        case spades = "♠️", hearts = "♥️", diamonds = "♦️", clubs = "♣️"
+    }
+    
+    // nested Rank enumeration
+    enum Rank: Int {
+        case Two = 2, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king, ace
+        struct Values {
+            let first: Int, second: Int?
+        }
+        var values: Values {
+            switch self {
+            case .ace:
+                return Values(first: 1, second: 11)
+            case .jack, .queen, .king:
+                return Values(first: 10, second: nil)
+            default:
+                return Values(first: self.rawValue, second: nil)
+            }
+        }
+    }
+    
+    // BlackjackCard properties and methods
+    let rank: Rank, suit: Suit
+    var description: String {
+        var output = "suit is \(suit.rawValue),"
+        output += " value is \(rank.values.first)"
+        if let second = rank.values.second {
+            output += " or \(second)"
+        }
+        
+        return output
+    }
+}
+
+let aceOfSpades = BlackjackCard(rank: .ace, suit: .spades) // Uses the implicit memberwise initializer
+print("Ace of Spades: \(aceOfSpades.description)")
+
+// Referring to Nested Types
+let heartSymbol = BlackjackCard.Suit.hearts.rawValue
