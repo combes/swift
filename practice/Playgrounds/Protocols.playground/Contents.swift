e7393e1aaabaf37def411294dd91790361e4b354
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2017-01-16 13:19:05 -0600

    Completed learning about Swift Protocols.

--- practice/Playgrounds/Protocols.playground/Contents.swift
+++ practice/Playgrounds/Protocols.playground/Contents.swift
@@ -1,4 +1,7 @@
 //: Playground - noun: a place where people can play
+// Excerpt From: Apple Inc. “The Swift Programming Language (Swift 3).” iBooks. https://itun.es/us/jEUH0.l
+
+import Foundation // Needed for @objc
 
 // Property Requirements
 protocol SomeProtocol {
@@ -53,6 +56,376 @@ class LinearCongruentialGenerator: RandomNumberGenerator {
 let generator = LinearCongruentialGenerator()
 print("Here's a random number: \(generator.random())")
 print("And another one: \(generator.random())")
+
+// Mutating Methods Requirements
+protocol Togglable {
+    mutating func toggle()
+}
+
+enum OnOffSwitch: Togglable {
+    case off, on
+    mutating func toggle() {
+        switch self {
+        case .off:
+            self = .on
+        case .on:
+            self = .off
+        }
+    }
+}
+var lightSwitch = OnOffSwitch.off
+lightSwitch.toggle()
+
+// Initializer Requirements
+protocol SpecialProtocol {
+    init(someParameter: Int)
+}
+
+// Class Implementation of Protocol Initializer Requirements
+class SomeClass: SpecialProtocol {
+    // The use of the 'required' modifier ensures that you provide an explicit or inherited implementation
+    required init(someParameter: Int) {
+        
+    }
+}
+
+protocol NewProtocol {
+    init()
+}
+
+class SomeSuperClass {
+    init() {
+        
+    }
+}
+
+class SomeSubClass: SomeSuperClass, NewProtocol {
+    // "required" from SomeProtocol conformance; "override" from SomeSuperClass
+    required override init() {
+        
+    }
+}
+
+// Protocols as Types
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
+for _ in 1...5 {
+    print("Random dice roll is \(d6.roll())")
+}
+
+// Delegation
+protocol DiceGame {
+    var dice: Dice { get }
+    func play()
+}
+
+protocol DiceGameDelegate {
+    func gameDidStart(_ game: DiceGame)
+    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int)
+    func gameDidEnd(_ game: DiceGame)
+}
+
+class SnakesAndLadders: DiceGame {
+    let finalSquare = 25
+    let dice = Dice(sides: 6, generator: LinearCongruentialGenerator())
+    var square = 0
+    var board: [Int]
+    init() {
+        board = Array(repeating: 0, count: finalSquare + 1)
+        board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+        board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+    }
+    var delegate: DiceGameDelegate?
+    func play() {
+        square = 0
+        delegate?.gameDidStart(self)
+        gameLoop: while square != finalSquare {
+            let diceRoll = dice.roll()
+            delegate?.game(self, didStartNewTurnWithDiceRoll: diceRoll)
+            switch square + diceRoll {
+            case finalSquare:
+                break gameLoop
+            case let newSquare where newSquare > finalSquare:
+                continue gameLoop
+            default:
+                square += diceRoll
+                square += board[square]
+            }
+        }
+        delegate?.gameDidEnd(self)
+    }
+}
+
+class DiceGameTracker: DiceGameDelegate {
+    var numberOfTurns = 0
+    func gameDidStart(_ game: DiceGame) {
+        numberOfTurns = 0
+        if game is SnakesAndLadders {
+            print("Started a new game of Snakes and Ladders")
+        }
+        print("The game is using a \(game.dice.sides)-sided dice")
+    }
+    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int) {
+        numberOfTurns += 1
+        print("Rolled a \(diceRoll)")
+    }
+    func gameDidEnd(_ game: DiceGame) {
+        print("The game lasted for \(numberOfTurns) turns")
+    }
+}
+
+let tracker = DiceGameTracker()
+let game = SnakesAndLadders()
+game.delegate = tracker
+game.play()
+
+// Adding Protocol Conformance with an Extension
+protocol TextRepresentable {
+    var textualDescription: String { get }
+}
+
+extension Dice: TextRepresentable {
+    var textualDescription: String {
+        return "A \(sides)-sided dice"
+    }
+}
+
+let d12 = Dice(sides: 12, generator: LinearCongruentialGenerator())
+print(d12.textualDescription)
+
+extension SnakesAndLadders: TextRepresentable {
+    var textualDescription: String {
+        return "A game of Snakes and Ladders with \(finalSquare) squares"
+    }
+}
+
+print(game.textualDescription)
+
+// Declaring Protocol Adoption with an Extension
+// Handle types that already conforms to all of the requirements of a protocol.
+struct Hamster {
+    var name: String
+    var textualDescription: String {
+        return "A hamster named \(name)"
+    }
+}
+
+extension Hamster: TextRepresentable {}
+
+let simonTheHamsert = Hamster(name: "Simon")
+let somethingTextRepresentable: TextRepresentable = simonTheHamsert
+print(somethingTextRepresentable.textualDescription)
+
+// Types do not automatically adopt a protocol just by satisfying its requirements.
+// They must always explicitly declare their adoption of the protocol.
+
+// Collections of Protocol Types
+let things: [TextRepresentable] = [game, d12, simonTheHamsert]
+
+for thing in things {
+    print(thing.textualDescription)
+}
+
+// Protocol Inheritance
+protocol PrettyTextRepresentable: TextRepresentable {
+    var prettyTextualDescription: String { get }
+}
+
+extension SnakesAndLadders: PrettyTextRepresentable {
+    var prettyTextualDescription: String {
+        var output = textualDescription + ":\n"
+        for index in 1...finalSquare {
+            switch board[index] {
+            case let ladder where ladder > 0:
+                output += "▲ "
+            case let snake where snake < 0:
+                output += "▼ "
+            default:
+                output += "○ "
+            }
+        }
+        return output
+    }
+}
+
+print(game.prettyTextualDescription)
+
+// Class-Only Protocols
+protocol SomeClassOnlyProtocol: class, SomeProtocol {
+    
+}
+
+// Use a class-only protocol when the behavior defined by that protocol’s requirements assumes or requires
+// that a conforming type has reference semantics rather than value semantics.
+
+// Protocol Composition
+// Require a type to conform to multiple protocols at once.
+protocol Named {
+    var name: String { get }
+}
+protocol Aged {
+    var age: Int { get }
+}
+struct SpecialPerson: Named, Aged {
+    var name: String
+    var age: Int
+}
+func wishHappyBirthday(to celebrator: Named & Aged) {
+    print("Happy birthday, \(celebrator.name), you're \(celebrator.age)!")
+}
+let birthdayPerson = SpecialPerson(name: "Dr. Who", age: 1270)
+wishHappyBirthday(to: birthdayPerson)
+
+// Checking for Protocol Conformance
+
+// The 'is' operator returns true if an instance conforms to a protocol and returns false if it does not.
+// The 'as?' version of the downcast operator returns an optional value of the protocol’s type,
+// and this value is nil if the instance does not conform to that protocol.
+// The 'as!' version of the downcast operator forces the downcast to the protocol type
+// and triggers a runtime error if the downcast does not succeed.
+
+protocol HasArea {
+    var area: Double { get }
+}
+
+class Circle: HasArea {
+    let pi = 3.14159265358979323846
+    var radius: Double
+    var area: Double { return pi * radius * radius }
+    init(radius: Double) { self.radius = radius }
+}
+
+class Country: HasArea {
+    var area: Double
+    init(area: Double) { self.area = area }
+}
+
+class Animal {
+    var legs: Int
+    init(legs: Int) { self.legs = legs }
+}
+
+let objects: [AnyObject] = [
+    Circle(radius: 2.0),
+    Country(area: 243_610),
+    Animal(legs: 4)
+]
+
+for object in objects {
+    if let objectWithArea = object as? HasArea {
+        print("Area is \(objectWithArea.area)")
+    } else {
+        print("Something that doesn't have an area")
+    }
+}
+
+// Optional Protocol Requirements
+@objc protocol CounterDataSource {
+    @objc optional func increment(forCount count: Int) -> Int
+    @objc optional var fixedIncrement: Int { get }
+}
+
+class Counter {
+    var count = 0
+    var dataSource: CounterDataSource?
+    func increment() {
+        if let amount = dataSource?.increment?(forCount: count) {
+            count += amount
+        } else if let amount = dataSource?.fixedIncrement {
+            count += amount
+        }
+    }
+}
+
+
+// Example:
+class ThreeSource: NSObject, CounterDataSource {
+    let fixedIncrement = 3
+}
+
+var counter = Counter()
+counter.dataSource = ThreeSource()
+for _ in 1...4 {
+    counter.increment()
+    print(counter.count)
+}
+
+// Example:
+@objc class TowardsZeroSource: NSObject, CounterDataSource {
+    func increment(forCount count: Int) -> Int {
+        if count == 0 {
+            return 0
+        } else if count < 0 {
+            return 1
+        } else {
+            return -1
+        }
+    }
+}
+
+counter.count = -4
+counter.dataSource = TowardsZeroSource()
+for _ in 1...5 {
+    counter.increment()
+    print(counter.count)
+}
+
+// Protocol Extensions
+extension RandomNumberGenerator {
+    func randomBool() -> Bool {
+        return random() > 0.5
+    }
+}
+
+let generatorTwo = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+print("And here's a random Boolean: \(generator.randomBool())")
+
+// Providing Default Implementation
+extension PrettyTextRepresentable {
+    var prettyTextualDescription: String {
+        return textualDescription // Default implementation
+    }
+}
+
+// Adding Constraints to Protocol Extensions
+extension Collection where Iterator.Element: TextRepresentable {
+    var textualDescription: String {
+        let itemsAsText = self.map { $0.textualDescription }
+        return "[" + itemsAsText.joined(separator: ", ") + "]"
+    }
+}
+
+let murrayTheHamser = Hamster(name: "Murray")
+let morganTheHamster = Hamster(name: "Morgan")
+let mauriceTheHamster = Hamster(name: "Maurice")
+let hamsters = [murrayTheHamser, morganTheHamster, mauriceTheHamster]
+
+print(hamsters.textualDescription)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
