cd199dd56f005e7a338639e7a43594e4c8012bce
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2017-03-15 13:30:00 -0500

    Added assets. Added enable state of buttons for start/stop operations.

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -11,10 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!    
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+        recordingLabel.sizeToFit()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,11 +28,14 @@ class ViewController: UIViewController {
 
     @IBAction func recordAudio(_ sender: Any) {
         recordingLabel.text = "Recording in Progress"
-        recordingLabel.sizeToFit()
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
     
     @IBAction func stopRecording(_ sender: Any) {
-        print("stop")
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
     }
 }
 
