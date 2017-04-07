04328f37f3405a8bcd67637669e0edab40c56e4c
Christopher Combes <ChristopherMichaelCombes@gmail.com>
2017-04-07 13:26:31 -0500

    Solved problem 1 of Project Euler. Working on a more efficient solution.

---
+++ ProjectEuler/Problem-1.playground/Contents.swift
@@ -0,0 +1,26 @@
+/*
+ Problem 1
+ 
+ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ 
+ Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+/// Find sum of all multiples of 3 or 5.
+///
+/// - Parameter max: Maximum value
+/// - Returns: Sum of all multiples
+func findSumAllMultiples(max: Int) -> Int {
+    var sum = 0
+    
+    // Natural numbers are the counting numbers as 1, 2, 3, etc.
+    for i in 1..<max {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i
+        }
+    }
+    
+    return sum
+}
+
+assert(findSumAllMultiples(max: 1000) == 233168)
